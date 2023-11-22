b06751ce4230f2e70068054af595779d687545c8
Aaron <dudfielda@gmail.com>
2023-11-22 07:01:12 +0000

    Update forms.py
    Notes added

--- ProfilesApp/forms.py
+++ ProfilesApp/forms.py
@@ -10,7 +10,7 @@ class UserRegistrationForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email')
-
+#clean password
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
