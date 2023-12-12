20fe4decec4deca5b4d562b73c1ea61bc312b3a2
Aaron <dudfielda@gmail.com>
2023-12-12 08:09:59 +0000

    Registration now working.

---
+++ LiveJam/Purpose.md
@@ -0,0 +1,32 @@
+# LiveJam Application
+
+## Overview
+
+LiveJam is an innovative application designed for musicians and bands to interact and update music chords in real-time. Leveraging the power of ChatGPT, users can easily modify chords, request key changes, and give instructions to band members, enhancing the collaboration and planning process for live performances and rehearsals.
+
+## Features
+
+- **Real-Time Chord Updates**: Users can update chords in real-time, allowing for dynamic collaboration during jam sessions. This is achieved after the initial request for certain conditions are made. They are then 'hardcoded' after being parsed from ChatGPT and create an editable chordsheet which can randomize chords, request changes for individual musicians (for example, someone could press a button that says 'Drums', and could have a button that says 'half time', which sends an instruction to the drummer). This way the entire band can interact. One request at a tim. 
+- **ChatGPT Integration**: Utilize ChatGPT for natural language processing, enabling users to input commands and requests conversationally.
+- **Interactive Commands**: Request key changes, chord alterations, and specific actions from band members, like playing in half time or incorporating triplets.
+- **Versatile Use Cases**: Suitable for live jam sessions or pre-planning for band practices and performances.
+
+## How to Use
+
+1. **Starting a Session**: Log in and start a new jam session. Invite your band members to join in.
+2. **Updating Chords**: Type in your chord updates or requests in the ChatGPT interface. The changes will appear on everyone’s screen in real time.
+3. **Band Member Requests**: Communicate specific instructions for band members through the chat interface. Examples include tempo changes, style suggestions, or focus areas.
+4. **Session Planning**: Use LiveJam to plan out your session beforehand, outlining chord progressions and band member roles.
+
+## Contributing
+
+We welcome contributions from the community. Whether it's improving the code, bug reporting, or feature suggestions, we appreciate your input. See `CONTRIBUTING.md` for more details on how to contribute.
+
+## Support
+
+For support, please email support@livejam.com or visit our support forum at `www.livejam.com/support`.
+
+## License
+
+LiveJam is licensed under the [MIT License](LICENSE).
+
--- NewUserApp/models.py
+++ NewUserApp/models.py
@@ -1,3 +1,85 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+from django.db.models import JSONField
+
+class Genre(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ArtistProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=128, blank=True)
+    last_name = models.CharField(max_length=128, blank = True)
+    stage_name = models.CharField(max_length=128, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+    instagram_handle = models.CharField(max_length=128, blank=True, null=True)  # JSON structure for social media links
+    facebook_link = models.CharField(max_length=128, blank=True, null=True)  # JSON structure for social media links
+    email = models.EmailField(max_length=254)
+    
+    def __str__(self):
+        return self.stage_name or self.user.username
+    
+class ArtistAddress(models.Model):
+    line1 = models.CharField(max_length=128, verbose_name='Address Line 1')
+    line2 = models.CharField(max_length=128, verbose_name='Address Line 2', blank=True, null=True)
+    town_or_city = models.CharField(max_length=64, verbose_name='Town/City')
+    county = models.CharField(max_length=64, verbose_name='County')
+    postcode = models.CharField(max_length=10, verbose_name='Postcode')
+    country = models.CharField(max_length=50, verbose_name='Country', default='United Kingdom')
+
+    def __str__(self):
+        address = f"{self.line1}"
+        if self.line2:
+            address += f", {self.line2}"
+        address += f", {self.town_or_city}, {self.county}, {self.postcode}, {self.country}"
+        return address
+
+class ArtistAdditionalDetails(models.Model):
+    artist_profile = models.OneToOneField(ArtistProfile, on_delete=models.CASCADE, related_name='additional_details')
+    genres = models.ManyToManyField(Genre)
+    artist_address = models.ForeignKey(ArtistAddress, on_delete=models.SET_NULL, null=True, blank=True)
+    experience_years = models.IntegerField(default=0)
+    awards = models.TextField(blank=True)
+    education = models.TextField(blank=True)
+    audio_video_samples = models.JSONField(blank=True, null=True)  # Links to audio/video samples
+    photo_gallery = models.JSONField(blank=True, null=True)  # Links to gallery images
+    availability_schedule = models.JSONField(blank=True, null=True)  # JSON structure for availability
+    preferred_venues = models.TextField(blank=True)
+    travel_willingness = models.BooleanField(default=False)
+    equipment_needs = models.TextField(blank=True)
+    technical_rider = models.TextField(blank=True)
+    rate_information = models.TextField(blank=True)
+    payment_preferences = models.TextField(blank=True)
+    covid_vaccination_status = models.BooleanField(default=False)
+    dietary_preferences = models.TextField(blank=True)
+    insurance_details = models.TextField(blank=True)
+    contractual_preferences = models.TextField(blank=True)
+    languages_spoken = models.CharField(max_length=256, blank=True)
+    personal_interests = models.TextField(blank=True)
+
+
+#This is a review model for clients to feedback to the artist & use in machine learning
+class Review(models.Model):
+    artist = models.ForeignKey(ArtistProfile, on_delete=models.CASCADE, related_name='reviews')
+    reviewer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)  # Assuming User is the model for reviewers
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    rating = models.PositiveIntegerField()  # Alternatively, use a DecimalField for more precision
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    #Specific responses stored in JSON format in database
+    questions_responses = JSONField(blank=True, null=True)  # Use a JSONField to store structured question & answer data
+
+    def __str__(self):
+        return f"{self.title} by {self.reviewer.username}"
+
+
